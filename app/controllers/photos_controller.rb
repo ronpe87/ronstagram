75b478fcec0a045cac274a307c781b62f594c35a
kk <>
2021-04-11 10:10:20 +0900

    photos/index作成

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,6 @@
 class PhotosController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
   def index
     @photos = Photo.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'photos#index'
 
-  resources :photo, only: [:show]
+  resources :photos
   resource :timeline, only: [:show]
 
   resource :profile, only: [:show, :update]
