eff9b0d837357b296888cd139aba351cd3299101
kk <>
2021-04-30 18:15:45 +0900

    relationテーブル作成follow機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,27 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   def avatar_image
     if profile&.avatar&.attached?
       profile.avatar
     else
       'default-avatar.png'
     end
+  end
+
+  def follow!(user)
+    following_relationships.create!(following_id: user.id)
+  end
+
+  def unfollow!(user)
+    relation = following_relationships.find_by!(following_id: user.id)
+    relation.destroy!
   end
 
   def prepare_profile
