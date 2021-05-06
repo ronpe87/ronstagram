b05a1d25c8be966d55eec9d020dd630d9b1b8232
kk <>
2021-05-06 22:42:18 +0900

    timelineいいね順で表示

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -4,5 +4,6 @@ class TimelinesController < ApplicationController
   def show
     user_ids = current_user.followings.pluck(:id)
     @photos = Photo.where(user_id: user_ids)
+    @ranks = Photo.joins(:likes).group(:id).order("count(likes.user_id) desc").limit(5)
   end
 end
