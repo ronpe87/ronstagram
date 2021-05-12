803e50c16e611308ed4e14ec190f3394a4d1f253
kk <>
2021-05-12 11:20:16 +0900

    commentを別のjsに移動

---
+++ app/javascript/packs/comment.js
@@ -0,0 +1,59 @@
+import $ from 'jquery'
+import axios from 'axios'
+import { csrfToken } from 'rails-ujs'
+
+axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
+
+const handleCommentForm = () => {
+  $('.show-comment-form').on('click', () => {
+    $('.show-comment-form').addClass('hidden')
+    $('.comment-text-area').removeClass('hidden')
+  })
+}
+
+const appendNewComment = (comment) => {
+  $('.comments-container').append(
+    `<div class="comment_detail">
+      <div class="comments_user">
+        <img class='comments_user_avatar' src="${comment.user.avatar_comment_image}">
+      </div>
+      <div class="comments_name">
+        <p>${comment.user.username}</p>
+      </div>
+      <div class="photo-comment">
+        <p>${comment.content}</p>
+      </div>
+    </div>`
+  )
+}
+
+document.addEventListener('turbolinks:load', () => {
+  const dataset = $('#photo-show').data()
+  const photoId = dataset.photoId
+
+  axios.get(`/photos/${photoId}/comments`)
+    .then((response) => {
+      const comments = response.data
+      comments.forEach((comment) => {
+        appendNewComment(comment)
+      })
+    })
+
+  handleCommentForm()
+
+  $('.add-comment-button').on('click', () => {
+    const content = $('#comment_content').val()
+    if (!content) {
+      window.alert('コメントを入力してください')
+    } else {
+      axios.post(`/photos/${photoId}/comments`, {
+        comment: { content: content }
+      })
+        .then((res) => {
+          const comment = res.data
+          appendNewComment(comment)
+          $('#comment_content').val('')
+        })
+    }
+  })
+})
