89c14b2a9f415aa039bd29fe53626965ef32d7bf
kk <>
2021-05-17 00:00:30 +0900

    module導入

--- config/routes.rb
+++ config/routes.rb
@@ -8,17 +8,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'photos#index'
 
-  resource :timeline, only: [:show]
-
   resources :accounts, only: [:show] do
     resources :followings, only: [:index]
     resources :follows, only: [:index, :show, :create]
     resources :unfollows, only: [:create]
   end
 
-  resource :timeline, only: [:show]
-
-  resource :profile, only: [:show, :update]
+  scope module: :apps do
+    resource :timeline, only: [:show]
+    resource :profile, only: [:show, :update]
+  end
 
   namespace :api, defaults: {format: :json} do
     scope '/photos/:photo_id' do
