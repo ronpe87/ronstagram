f374a555c324086d49b61821160aefb16f6eda8a
kk <>
2021-06-08 21:30:53 +0900

    コメントしたら投稿主にsendgrid経由でメールが届くようにした。

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,6 +7,7 @@ class Comment < ApplicationRecord
 
   private
   def send_email
-    SendEmail.test_email
+    SendEmail.test_email(photo) #開発環境、Herokuどちらでもメール画面にて閲覧可能
+    CommentMailer.new_comment(photo).deliver_later #開発環境でletter_openerにて閲覧可能
   end
 end
